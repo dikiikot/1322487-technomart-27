57a4e1da46fed110a8dd8d1e576b8469cfdfc1f0
Alexander <al.borodin88@gmail.com>
2020-03-16 22:26:11 +0300

    промежуток

--- js/script.js
+++ js/script.js
@@ -6,13 +6,11 @@ var form = popup.querySelector("form");
 var email = popup.querySelector("[name=email]");
 var isStorageSupport = true;
 var storage = "";
-
 try {
   storage = localStorage.getItem("login");
 } catch (err) {
   isStorageSupport = false;
 }
-
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
@@ -23,12 +21,10 @@ link.addEventListener("click", function(evt) {
     login.focus();
   }
 });
-
 close.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
 });
-
 form.addEventListener("submit", function(evt) {
   if (!login.value || !email.value) {
     evt.preventDefault();
@@ -50,15 +46,12 @@ window.addEventListener("keydown", function(evt) {
 //карта
 var link = document.querySelector(".contacts-map");
 var mapLink = document.querySelector(".contacts-map");
-
 var mapPopup = document.querySelector(".modal-map");
 var mapClose = mapPopup.querySelector(".modal-close");
-
 mapLink.addEventListener("click", function(evt) {
   evt.preventDefault();
   mapPopup.classList.add("modal-show");
 });
-
 mapClose.addEventListener("click", function(evt) {
   evt.preventDefault();
   mapPopup.classList.remove("modal-show");
@@ -71,12 +64,9 @@ window.addEventListener("keydown", function(evt) {
     }
   }
 });
-
 var linkB = document.querySelectorAll(".test777");
-var popupB= document.querySelector(".modal-bucket");
+var popupB = document.querySelector(".modal-bucket");
 var closeB = popupB.querySelector(".modal-close");
-
-
 linkB.addEventListener("click", function(evt) {
   evt.preventDefault();
   popupB.classList.add("modal-show");
